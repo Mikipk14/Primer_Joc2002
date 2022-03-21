4f8049b1e1b6ac34f7076810f1b8a063881a8b98
Mikipk14 <101257345+Mikipk14@users.noreply.github.com>
2022-03-21 14:25:50 +0100

    Implementació de moviments dins de la força de la gravetat. principi de sprite animació

--- Escenes/Cossos/KinematicBody2D.gd
+++ Escenes/Cossos/KinematicBody2D.gd
@@ -1,10 +1,20 @@
 extends KinematicBody2D
-var velocitat_i = 200
+var velocitat_i = 300
 var velocitat = Vector2.ZERO
 
 var A_baix = Vector2.DOWN
 var Gravetat = Vector2.DOWN * 980
 
 func _physics_process(delta):
-	move_and_slide(velocitat)
 	velocitat += Gravetat *delta
+	velocitat.x = 0
+	
+	if Input.is_action_pressed("Dreta"):
+		velocitat += Vector2.RIGHT * velocitat_i
+	if Input.is_action_pressed("Esquerra"):
+		velocitat += Vector2.LEFT * velocitat_i
+	if Input.is_action_just_pressed("A dalt") and is_on_floor():
+		velocitat += Vector2.UP * velocitat_i*1.5
+
+	velocitat = move_and_slide(velocitat, Vector2.UP)
+
